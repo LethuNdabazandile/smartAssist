d3b74e5798a562ee2cfb93cbf12888c079a17d9f
LethuNdabazandile <lethu@appimate.com>
2022-11-27 07:29:53 +0200

    home page miner editions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 
-import { IonAlert, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, 
+import { IonAlert, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonContent, IonHeader, IonIcon, 
     IonImg, 
     IonItem, 
     IonLabel, 
@@ -478,9 +478,27 @@ const Home:React.FC<any> = ({routerRef, doPlay})=>{
                     </form>
                     {
                         (careerMSG !=="")?(
+                        <>
                         <IonCard>
                             <p style={{color: myColor}}>{careerMSG}</p>
                         </IonCard>
+                        <h3 style={{textAlign:"center"}}>In demand jobs</h3>
+                        <IonCard>
+                            <IonCardHeader>
+                                <IonCardSubtitle>Data Scientists</IonCardSubtitle>                            
+                            </IonCardHeader>
+                        </IonCard>
+                        <IonCard>
+                            <IonCardHeader>
+                                <IonCardSubtitle>Software developers</IonCardSubtitle>                            
+                            </IonCardHeader>
+                        </IonCard>
+                        <IonCard>
+                            <IonCardHeader>
+                                <IonCardSubtitle>Data engeneeres</IonCardSubtitle>                            
+                            </IonCardHeader>
+                        </IonCard>
+                        </>
                         ):("")
                     }
                     
